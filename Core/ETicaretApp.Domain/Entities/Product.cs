ad1077b71255ff41db4a2e068d794e21041b1e82
culinalionur <onurakinci53@gmail.com>
2023-02-08 10:55:41 +0300

    Add project files.

---
+++ Core/ETicaretApp.Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using ETicaretApp.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Domain.Entities
+{
+    public class Product : BaseEntity
+    {
+
+    }
+}
---
+++ Infrastructure/ETicaretApp.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Persistence.Concretes
+{
+    internal class ProductService
+    {
+    }
+}
