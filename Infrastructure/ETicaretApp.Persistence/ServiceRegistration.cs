e2ed074d04c2de6ba4b3b06fae0234a1a96daf3a
culinalionur <onurakinci53@gmail.com>
2023-02-09 20:38:19 +0300

    Generic Repository added

--- Infrastructure/ETicaretApp.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretApp.Persistence/ServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using ETicaretApp.Persistence.Contexts;
+﻿using ETicaretApp.Application.Repositories;
+using ETicaretApp.Persistence.Contexts;
+using ETicaretApp.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -13,8 +15,13 @@ namespace ETicaretApp.Persistence
     {
         public static void AddPersistenceService(this IServiceCollection services)
         {
-            services.AddDbContext<ETicaretAPIDbContext>(opt =>
-            opt.UseSqlServer(Configuration.ConnectionString));
+            services.AddDbContext<ETicaretAPIDbContext>(opt => opt.UseSqlServer(Configuration.ConnectionString));
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository , CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
---
+++ Presentation/ETicaretApp.Api/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretApp.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+       
+
+    }
+}
