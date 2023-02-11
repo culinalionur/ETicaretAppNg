f82a47e3051013dd762b2d02c654561b12167c71
culinalionur <onurakinci53@gmail.com>
2023-02-11 10:56:42 +0300

    Mediator Pattern started

---
+++ Core/ETicaretApp.Application/Features/Queries/GetAllProducts/GetAllProductQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretApp.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretApp.Application.Features.Queries.GetAllProducts
+{
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, GetAllProductQueryResponse>
+    {
+        private readonly IProductReadRepository _productReadRepository;
+
+        public GetAllProductQueryHandler(IProductReadRepository productReadRepository)
+        {
+            _productReadRepository = productReadRepository;
+        }
+
+        public Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
